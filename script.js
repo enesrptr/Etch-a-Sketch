98790242f34b5854f753532bfd2ca7545adb0a8e
enesrptr <enesmalikyildiz8@gmail.com>
2024-12-25 01:06:36 +0300

    'mouseover' and 'mouseout' event types has been added

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const container = document.getElementById("container");
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+
+  
+  for (let c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+    cell.innerText = c + 1;
+    cell.classList.add("grid-item"); 
+    container.appendChild(cell);
+  }
+
+  
+  const newCells = container.querySelectorAll(".grid-item"); 
+  newCells.forEach((cell) => {
+    
+    cell.addEventListener("mouseover", () => {
+      cell.style.color = "red"; 
+    });
+    
+    
+    cell.addEventListener("mouseout", () => {
+      cell.style.color = ""; 
+    });
+  });
+}
 
 makeRows(16, 16);
