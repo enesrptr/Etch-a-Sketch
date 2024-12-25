12c71d7ccf71704633e54fac6acbacd6f591283f
enesrptr <enesmalikyildiz8@gmail.com>
2024-12-26 00:39:32 +0300

    A button has been added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
+let row = 0;
+let col = 0;
+let clickCount = 0; // variable for reload the page
+
 const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
+
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
 
@@ -27,4 +33,49 @@ function makeRows(rows, cols) {
   });
 }
 
-makeRows(16, 16);
+// The function that make efficient the button then run the other functions
+function popUp() {
+
+  clickCount++;
+
+  if(clickCount === 1){
+    let row = prompt("Please enter the row");
+    let col = prompt("Please enter the col");
+    row = parseInt(row);
+    col = parseInt(col);
+  
+    checkInteger(row, col);
+    makeRows(row, col);
+  }
+
+  else if (clickCount === 2) {
+    location.reload(); 
+  }
+  
+}
+
+// Validation for integer
+function checkInteger(row, col) {
+  try {
+    
+    if (isNaN(row) || isNaN(col)) {
+      throw new Error("Both inputs must be numbers.");
+    }
+
+    if (row < 1 || row > 100 || col < 1 || col > 100) {
+      throw new Error("Inputs must be within the range of 1 to 100.");
+    }
+
+    if (row !== col) {
+      throw new Error("Row and column must be the same.");
+    }
+
+    console.log("Row and column values are valid:", row, col);
+  } catch (error) {
+    document.getElementById("errorMessage").innerText = "Error: " + error.message; //It shows the error on the page
+  }
+}
+
+
+
+
